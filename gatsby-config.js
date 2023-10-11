b476ed1c6da8a5d7e25fa8e55684cafd4b853679
Aditya Ranganath <aditya.ranganath1@gmail.com>
2023-10-11 16:52:31 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Video-to-Text`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
